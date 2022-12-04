cfc304935ae611155f74ed346661c1437828d218
William Horn <william.horn.dev@gmail.com>
2022-12-04 15:09:17 -0500

    Updated contained function name (again)

--- 2022/day4/day4.py
+++ 2022/day4/day4.py
@@ -11,7 +11,7 @@ def process_input():
         return section_input
 
 
-def section_contained(left, right, func):
+def sections_contained(left, right, func):
     left_range  = range(left[0],  left[1]  + 1)
     right_range = range(right[0], right[1] + 1)
     return func(section in left_range for section in right_range) or \
@@ -21,7 +21,7 @@ def section_contained(left, right, func):
 def solution(sections_input, func):
     num_contained = 0
     for sections in sections_input:
-        if section_contained(*sections, func):
+        if sections_contained(*sections, func):
             num_contained += 1
     return num_contained
 
